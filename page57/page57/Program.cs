22882379127380a3f4d33fda3ec7d39c5977459f
Kun Qian <leo.qiankun@gmail.com>
2019-03-15 18:23:10 -0700

    Initial commit

---
+++ page57/page57/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace page57
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+            Console.WriteLine("What course are you on?");
+            string courseName = Console.ReadLine();
+            Console.WriteLine("What page number?");
+            string pageNumber = Console.ReadLine();
+            Console.WriteLine("Do you need help with anything? Please answer “true” or “false”.");
+            string ifneedHelp = Console.ReadLine();
+            Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
+            string positiveExp = Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+            string feeback = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            string hours = Console.ReadLine();
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+            Console.ReadLine();
+        }
+    }
+}
