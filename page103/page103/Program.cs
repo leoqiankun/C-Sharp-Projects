ce1627757db443dea871cdb2af8baf116bdd5142
Kun Qian <leo.qiankun@gmail.com>
2019-03-21 14:10:36 -0700

    page 103

---
+++ page103/page103/Program.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace page103
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            //string[] texts = { "asdf", "qwer", "zxcv", "uiop", "hjkl" };
+            //Console.WriteLine("Type in some texts");
+            //string input = Console.ReadLine();
+            //for (int i = 0; i < texts.Length; i++)
+            //{
+            //    texts[i] = texts[i] + input;
+            //    Console.WriteLine(texts[i]);
+            //}            
+            //Console.ReadLine();
+
+            //2
+            //int[] numbers = { 1, 23, 345, 67, 23 };
+            //for (int i = 0; i < 5;)
+            //{
+            //    Console.WriteLine(numbers[i]);
+            //}
+            //Console.ReadLine();
+
+            //3
+            //int[] numbers = { 1, 23, 345, 67, 23 };
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine(numbers[i]);
+            //}
+            //Console.ReadLine();
+
+            //4
+            //List<int> numbers = new List<int> { 1, 23, 345, 67, 23 };
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine(numbers[i]);
+            //}
+            //Console.ReadLine();
+
+            //5
+            //List<string> names = new List<string> { "James", "Lee", "Ken", "Conner", "Kate" };
+            //for (int i = 1; i <= 3; i++)
+            //{
+            //    Console.WriteLine(names[i]);
+            //}
+            //Console.ReadLine();
+
+            //6
+            //List<string> names = new List<string> { "James", "Lee", "Ken", "Conner", "Kate" };
+            //Console.WriteLine("Please type one of the names from the list for a number: James, Lee, Ken, Conner, Kate");
+            //string input = Console.ReadLine();
+            //for (int i = 0; i < names.Count; i++)
+            //{
+            //    if (names[i] == input)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+            //Console.ReadLine();
+
+            //7
+            //List<string> names = new List<string> { "James", "Lee", "Ken", "Conner", "Kate" };
+            //Console.WriteLine("Please type one of the names from the list for a number: James, Lee, Ken, Conner, Kate");
+            //string input = Console.ReadLine();
+            //int j = 0;
+            //for (int i = 0; i < names.Count; i++)
+            //{
+            //    if (names[i] == input)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //    else
+            //    {
+            //        j++;
+            //    }   
+            //}
+            //if (j == 5)
+            //{
+            //    Console.WriteLine("What you just typed is not in the list.");
+            //}
+            //Console.ReadLine();
+
+            //8
+            //List<string> names = new List<string> { "James", "Lee", "Ken", "Conner", "Kate" };
+            //Console.WriteLine("Please type one of the names from the list for a number: James, Lee, Ken, Conner, Kate");
+            //string input = Console.ReadLine();
+            //int j = 0;
+            //for (int i = 0; i < names.Count; i++)
+            //{
+            //    if (names[i] == input)
+            //    {
+            //        Console.WriteLine(i);
+            //        Console.ReadLine();
+            //        return;
+            //    }
+            //    else
+            //    {
+            //        j++;
+            //    }
+            //}
+            //if (j == 5)
+            //{
+            //    Console.WriteLine("What you just typed is not in the list.");
+            //}
+
+            //Console.ReadLine();
+
+            //9
+            //List<string> sports = new List<string> { "football", "tennis", "football", "golf", "basketball", "swimming" };
+            //Console.WriteLine("Please choose your favorite sport from the list: tennis, football, golf, basketball and swimming.");
+            //string input = Console.ReadLine();
+            //for (int i = 0; i < sports.Count; i++)
+            //{
+            //    if (sports[i] == input)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+            //Console.ReadLine();
+
+            //10
+            //List<string> sports = new List<string> { "football", "tennis", "football", "golf", "basketball", "swimming" };
+            //Console.WriteLine("Please choose your favorite sport from the list: tennis, football, golf, basketball and swimming.");
+            //string input = Console.ReadLine();
+            //int j = 0;
+            //for (int i = 0; i < sports.Count; i++)
+            //{
+            //    if (sports[i] == input)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //    else
+            //    {
+            //        j++;
+            //    }
+            //}
+            //if (j == sports.Count)
+            //{
+            //    Console.WriteLine("Your choice is not in the list.");
+            //}
+            //Console.ReadLine();
+
+            //11
+            //List<string> names = new List<string> { "Bob", "Jane", "Liam", "Mary", "Alex", "Bob" };
+            
+            //foreach (string name in names)
+            //{
+            //    int n = 0;
+            //    for (int i = 0; i < names.Count; i++)
+            //    {
+                    
+            //        if (name == names[i])
+            //        {
+            //            n++;
+            //        }
+            //    }
+            //    if (n == 1)
+            //    {
+            //        Console.WriteLine("There is only one " + name + " in the list.");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("There are more than one " + name + " in the list.");
+            //    }
+                
+            //}
+            //Console.ReadLine();
+        }
+    }
+}
