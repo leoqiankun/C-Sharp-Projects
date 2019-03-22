7b5a3e8d7af9ab730bb00941f6d8f3db400b8ee6
Kun Qian <leo.qiankun@gmail.com>
2019-03-22 12:51:30 -0700

    page 113

---
+++ page113/page113/Class.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace page113
+{
+    class Class
+    {
+        public int primeNum { get; set; }
+
+        public static void test(int num1, int num2)
+        {
+            int result = num1 * 100 / 2;
+            Console.WriteLine("The second number is {0}", num2);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ page113/page113/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace page113
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class num1 = new Class();
+            num1.primeNum = 5;
+            Class num2 = new Class();
+            num2.primeNum = 11;
+
+            Class.test(num1:num1.primeNum, num2:num2.primeNum);
+            
+        }
+    }
+}
