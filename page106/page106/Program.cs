0193206410fb659faea7034c956afb622722edb4
Kun Qian <leo.qiankun@gmail.com>
2019-03-22 08:59:53 -0700

    page 106

--- page106/page106/Program.cs
+++ page106/page106/Program.cs
@@ -10,6 +10,40 @@ namespace page106
     {
         static void Main(string[] args)
         {
+            try
+            {
+                List<int> numbers = new List<int> { 123, 45, 78, 4, 11 };
+                Console.WriteLine("Please enter a number:");
+                int divisor = Convert.ToInt32(Console.ReadLine());
+                foreach (int number in numbers)
+                {
+                    int quotient = number / divisor;
+                    Console.WriteLine(quotient);
+                }
+                Console.ReadLine();
+            }
+
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
         }
+        
+
     }
 }
