33fb2414060404b9c67f070dd57f0c96952cbfd0
Kun Qian <leo.qiankun@gmail.com>
2019-03-22 10:42:44 -0700

    page 111

---
+++ page111/page111/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace page111
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type in an integer for some operations: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            int result1 = operations.operation1(input);
+            Console.WriteLine("Your first result is " + result1);
+            int result2 = operations.operation2(input);
+            Console.WriteLine("Your second result is " + result2);
+            int result3 = operations.operation3(input);
+            Console.WriteLine("Your third result is " + result3);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ page111/page111/operations.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace page111
+{
+    class operations
+    {
+        
+
+            public static int operation1(int num)
+            {
+                int result = num * 100;
+                return result;
+            }
+
+            public static int operation2(int num)
+            {
+                int result = num / 2;
+                return result;
+            }
+
+            public static int operation3(int num)
+            {
+                int result = num * 100 / 2;
+                return result;
+            }
+        
+    }
+}
