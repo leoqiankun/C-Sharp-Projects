dced3acab60a3ef500974900e666aa95900b131e
Kun Qian <leo.qiankun@gmail.com>
2019-03-22 13:35:17 -0700

    page 114

---
+++ page114/page114/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace page114
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class Class1 = new Class();
+            Class1.Int = 12;
+
+            int num1 = Class.method(Class1.Int);
+            Console.WriteLine(num1);
+            
+
+            Class Class2 = new Class();
+            Class2.Double = 12.13;
+
+            int num2 = Class.method(Class2.Double);
+            Console.WriteLine(num2);
+            Console.ReadLine();
+
+            Class Class3 = new Class();
+            Class3.String = "50";
+
+            int num3 = Class.method(Class3.String);
+            Console.WriteLine(num3);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ page114/page114/class.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace page114
+{
+    class Class
+    {
+        public int Int { get; set; }
+        public double Double { get; set; }
+        public string String { get; set; }
+
+        public static int method(int num)
+        {
+            int result = num * 2;
+            return result;
+        }
+
+        public static int method(double num)
+        {
+            double result = num / 2;
+            return Convert.ToInt32(result);
+        }
+
+        public static int method(string num)
+        {
+            int result = Convert.ToInt32(num) + 2;
+            return result;
+        }
+
+    }
+
+        
+}
