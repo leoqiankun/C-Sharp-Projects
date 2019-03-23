ac7eb2bd232b07e5a516b019c9e3869fe8b2031d
Kun Qian <leo.qiankun@gmail.com>
2019-03-23 13:07:36 -0700

    page 120

---
+++ page120/page120/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace page120
+{
+    public class Person
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+
+        public void SayName()
+        {
+            Console.WriteLine("Name: [" + FirstName + " " + LastName + "]");
+        }
+    }
+}
