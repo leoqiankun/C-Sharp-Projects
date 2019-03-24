bad2f0d7632f7438be550c3bd64fa177a8b3deb1
Kun Qian <leo.qiankun@gmail.com>
2019-03-24 14:54:53 -0700

    page 129

---
+++ page129/page129/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace page129
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee p1 = new Employee() { ID = 234 };
+            Employee p2 = new Employee() { ID = 223 };
+            Employee p3 = new Employee() { ID = 234 };
+
+           
+
+            Console.WriteLine(p1.ID == p2.ID);
+            Console.WriteLine(p1.ID == p3.ID);
+            Console.WriteLine(p2.ID == p3.ID);
+            Console.ReadLine();
+        }
+    }
+}
