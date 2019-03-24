e042ebc7cd198dcc83413808bbb13a79b069828e
Kun Qian <leo.qiankun@gmail.com>
2019-03-24 13:20:41 -0700

    page 126

---
+++ page126/page126/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace page126
+{
+    public class Employee : IQuittable
+
+    {
+        public void Quit()
+        {
+            Console.WriteLine("Interfaces");
+        }
+
+    }
+}
---
+++ page126/page126/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace page126
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IQuittable quit = new Employee();
+
+            quit.Quit();
+            Console.ReadLine();
+
+        }
+    }
+}
