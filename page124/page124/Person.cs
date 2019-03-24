5cfb4c030dff841417570d01112df01ae6c30812
Kun Qian <leo.qiankun@gmail.com>
2019-03-24 12:56:11 -0700

    page 124

---
+++ page124/page124/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace page124
+{
+    public abstract class Person
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+
+        public virtual void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+        }
+
+    }
+}
---
+++ page124/page124/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace page124
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee() { LastName = "Student", FirstName = "Sample" };
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
