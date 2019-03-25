54da7039a6315e6f7db99fa1274a9a5ee5e58db7
Kun Qian <leo.qiankun@gmail.com>
2019-03-24 18:05:55 -0700

    page 131

--- page131/page131/Program.cs
+++ page131/page131/Program.cs
@@ -10,6 +10,22 @@ namespace page131
     {
         static void Main(string[] args)
         {
+            Employee<int> employee1 = new Employee<int>();
+            employee1.things = new List<int>() { 2, 4, 5 };
+
+            Employee<string> employee2 = new Employee<string>();
+            employee2.things = new List<string>() { "bob", "sam", "kate" };
+
+            foreach (int number in employee1.things)
+            {
+                Console.WriteLine(number);
+            }
+            foreach (string name in employee2.things)
+            {
+                Console.WriteLine(name);
+            }
+            Console.ReadLine();
+
         }
     }
 }
