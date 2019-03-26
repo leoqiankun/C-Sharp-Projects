27cd8ad81d4d132f740031ab085cff6069413ca4
Kun Qian <leo.qiankun@gmail.com>
2019-03-26 12:34:37 -0700

    page 137

---
+++ page137/page137/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace page137
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Amount = 12345.24356m;
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+
+        }
+    }
+}
