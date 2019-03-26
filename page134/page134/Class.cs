00b8e6eb8d155992416426630981fb6bfcbc5bdd
Kun Qian <leo.qiankun@gmail.com>
2019-03-26 12:08:33 -0700

    page 134

---
+++ page134/page134/Class.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace page134
+{
+    public enum Days
+    {
+        
+        
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        
+    }
+}
---
+++ page134/page134/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace page134
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter the current day of the week:");
+
+                string today = Console.ReadLine();
+
+                Days day = (Days)Enum.Parse(typeof(Days), today, true);
+
+                if (day == Days.Tuesday)
+                {
+                    Console.WriteLine("That's correct!");
+                }
+                else
+                {
+                    Console.WriteLine("That's incorrect!");
+                }
+
+                Console.ReadLine();
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}
