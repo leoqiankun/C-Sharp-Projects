72275bb0a7289384fa42335f8c80e08a7344d08a
Kun Qian <leo.qiankun@gmail.com>
2019-03-31 14:58:08 -0700

    add

---
+++ page34/page34/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace page34
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, we’re “accounting” on you!");
+            Console.ReadLine();
+        }
+    }
+}
