d2038da2f4f8832e330696974b7219972d061466
Kun Qian <leo.qiankun@gmail.com>
2019-03-31 14:54:58 -0700

    page 154

---
+++ page154/page154/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace page154
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a whole number:");
+            string number = Console.ReadLine();
+            File.AppendAllText(@"C:\Users\Kun\Desktop\log.txt", number + "\n");
+            Console.WriteLine(File.ReadAllText(@"C:\Users\Kun\Desktop\log.txt"));
+            Console.ReadLine();
+        }
+    }
+}
