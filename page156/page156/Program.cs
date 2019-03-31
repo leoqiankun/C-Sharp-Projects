dd5b493f065f44eb04183ae2eb9f33b1769f34a4
Kun Qian <leo.qiankun@gmail.com>
2019-03-31 15:54:41 -0700

    page 156

---
+++ page156/page156/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace page156
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Current date and timme: " + DateTime.Now + ".");
+            Console.WriteLine("Please enter a whole number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            DateTime dt = DateTime.Now;
+            dt = dt.AddHours(number);
+            Console.WriteLine("In {0} hours, the date and time will be {1}", number, dt);           
+            Console.ReadLine();
+        }
+    }
+}
