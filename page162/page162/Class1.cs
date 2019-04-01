cec56dac94171bc7c890c890619bfb0e90e4f423
Kun Qian <leo.qiankun@gmail.com>
2019-04-01 09:35:37 -0700

    page 162

---
+++ page162/page162/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace page162
+{
+    public class Class1
+    {
+        public Class1(string color) : this(color, 0)
+        {
+
+        }
+        public Class1(string color, int number)
+        {
+            colors = color;
+            size = number;
+        }
+        public string colors { get; set; }
+        public int size { get; set; }
+    }
+}
---
+++ page162/page162/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace page162
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string greeting = "Hello!";
+            var list = new List<int> { 3, 6, 9 };
+
+            Console.WriteLine(greeting);
+            foreach (int number in list)
+            {
+                Console.WriteLine(number);
+            }
+
+            Class1 class1 = new Class1("red");
+            Console.WriteLine(class1.colors + " " + class1.size);
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
