1674a8e1a0f2c807abf0e33d5493f2d6c59d42ac
Kun Qian <leo.qiankun@gmail.com>
2019-04-01 11:15:26 -0700

    page 165

---
+++ page165/page165/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace page165
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("How old are you?");
+                int age = Convert.ToInt32(Console.ReadLine());
+                if (age <= 0)
+                {
+                    throw new ArgumentException();
+                }
+                int year = DateTime.Now.Year - age;
+                Console.WriteLine("You were born in {0}.", year);
+                Console.ReadLine();
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Age can not be zero or under zero!");
+                Console.ReadLine();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The age you entered is not valid!");
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}
